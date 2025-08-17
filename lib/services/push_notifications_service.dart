53331acb295e201e9aea2b966fd57f6af91ee303
khaledgamal22 <kghaly56@gmail.com>
2025-08-17 21:22:54 +0300

    finish

--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -8,8 +8,6 @@ class PushNotificationsService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static Future init() async {
     messaging.requestPermission();
-    final String? token = await messaging.getToken();
-    log(token ?? "token is null");
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       LocalNotificationService.showBasicNotification(message: message);
@@ -18,6 +16,5 @@ class PushNotificationsService {
 
   static Future<void> handleBackgroundMessage(RemoteMessage message) async {
     await Firebase.initializeApp();
-    log(message.notification?.title ?? 'no notification');
   }
 }
